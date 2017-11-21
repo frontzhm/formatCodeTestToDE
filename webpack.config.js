a701c2086a8b40e673c4a694d49aa7a129ff8045
frontzhm <1143303516@qq.com>
2017-11-21 18:03:41 +0800

    添加css

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,15 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  // 每个module 用到的规则(针对什么文件使用什么loader)
+  module: {
+    rules: [
+      // css-loader让js文件里能引入css import './style.css';从后往前,style是在html里添加style标签
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 }
