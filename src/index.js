67f03b30c6d0b090a73ac270ffa5484d78729d0f
frontzhm <1143303516@qq.com>
2017-11-22 14:02:43 +0800

    多入口多出口

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import _ from 'lodash'
+import print from './print'
 function component () {
   var element = document.createElement('div')
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  var btn = document.createElement('button')
+  btn.innerHTML = 'click me and check console'
+  btn.onclick = print
+  element.appendChild(btn)
   return element
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,13 @@
 const path = require('path')
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  // entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
   },
-  // 每个module 用到的规则(针对什么文件使用什么loader)
-  module: {
-    rules: [
-      // css-loader让js文件里能引入css import './style.css';从后往前,style是在html里添加style标签
-      /**
-       * 现在，当该模块运行时，含有 CSS 字符串的 <style> 标签，将被插入到 html 文件的 <head> 中
-       */
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      // file-loader让js文件能引入img,import myImg from './icon.png', 这样在css里或html里写后面路径会自动转化成最终路径
-      /**
-       * 现在，当你 import MyImage from './my-image.png'，该图像将被处理并添加到 output 目录，
-       * 并且 MyImage 变量将包含该图像在处理后的最终 url。
-       * 当使用 css-loader 时，如上所示，你的 CSS 中的 url('./my-image.png') 会使用类似的过程去处理。
-       * loader 会识别这是一个本地文件，并将 './my-image.png' 路径，替换为输出目录中图像的最终路径。
-       * html-loader 以相同的方式处理 <img src="./my-image.png" />。
-       */
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: 'file-loader'
-      },
-      /**
-       * file-loader 和 url-loader 可以接收并加载任何文件，然后将其输出到构建目录。
-       * 这就是说，我们可以将它们用于任何类型的文件，包括字体。
-       *
-       */
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: 'file-loader'
-      }
-    ]
+  output: {
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
   }
 }
