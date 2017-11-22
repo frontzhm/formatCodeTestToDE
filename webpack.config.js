cf838a4cadd8db894b89b335f1c44032404e9b64
frontzhm <1143303516@qq.com>
2017-11-22 14:42:29 +0800

    清空dist插件

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 // 这个插件会在 output.path下生成html文件 默认生成的文件名是index.html,默认自动引入output.filename
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+// 每次运行命令的时候 先清除dist的其他文件
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
   // entry: './src/index.js',
   entry: {
@@ -14,6 +16,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output management'
     })
