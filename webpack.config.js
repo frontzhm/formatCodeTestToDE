8680e6c466e53f0b94857f9c63d986c77000fa27
frontzhm <1143303516@qq.com>
2017-11-22 14:18:41 +0800

    增加生成html的plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path')
+// 这个插件会在 output.path下生成html文件 默认生成的文件名是index.html,默认自动引入output.filename
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   // entry: './src/index.js',
   entry: {
+    // 多入口文件
     app: './src/index.js',
     print: './src/print.js'
   },
@@ -9,5 +12,10 @@ module.exports = {
     // filename: 'bundle.js',
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output management'
+    })
+  ]
 }
